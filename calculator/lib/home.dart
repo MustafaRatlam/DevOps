534631a44c12886887b11ee137f50014114110aa
Mustafa Ratlamwala <mustafaratlam110@gmail.com>
2021-06-27 19:08:34 +0500

    Calculator Done\

--- calculator/lib/home.dart
+++ calculator/lib/home.dart
@@ -1,59 +1,93 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 
-class Home extends StatefulWidget {
-  const Home({ Key? key }) : super(key: key);
+class Calculator extends StatefulWidget {
+ 
 
   @override
-  _HomeState createState() => _HomeState();
+  _CalculatorState createState() => _CalculatorState();
 }
 
-class _HomeState extends State<Home> {
+class _CalculatorState extends State<Calculator> {
+
+var result = "";
+
+Widget btn(var textt){
+  return ElevatedButton(onPressed: (){
+    setState(() {
+      result = result + textt;
+    });
+  }, child: Text(textt));
+}
+
+clear(){
+  setState(() {
+    result = "";
+  });
+}
+
+output(){
+  Parser p = Parser();
+  Expression exp = p.parse(result);
+  ContextModel cm = ContextModel();
+  double eval = exp.evaluate(EvaluationType.REAL, cm);
+
+  setState(() {
+    result = eval.toString();
+  });
+}
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
+      body: Center(
         child: Column(
-          children: [
-           Container(
-             height: 210,
-             color: Colors.blueGrey,
-             
-             child: ListView(
-               scrollDirection: Axis.horizontal,
-               shrinkWrap: true,
-               children: [
-                 
-                 Container(
-                   margin: EdgeInsets.only(top: 10),
-                   color: Colors.amberAccent,
-                   height: 200,
-
-                 ),
-                Container(
-                   margin: EdgeInsets.only(top: 10),
-                   color: Colors.amberAccent,
-                   height: 200,
-
-                 ), 
-                 Container(
-                   margin: EdgeInsets.only(top: 10),
-                   color: Colors.amberAccent,
-                   height: 200,
-
-                 ),
-                 Container(
-                   margin: EdgeInsets.only(top: 10),
-                   color: Colors.amberAccent,
-                   height: 200,
-
-                 ),
-               
-               ],
-             ),
-           )
-      
+          mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(result, style: TextStyle(
+                fontSize: 50, fontWeight: FontWeight.bold
+              ),),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                btn("1"),
+                btn("2"),
+                btn("3"),
+                btn("+"),
+            ],
+            ),
+             Row(
+               mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                btn("4"),
+                btn("5"),
+                btn("6"),
+                btn("-"),
+            ],
+            ),
+             Row(
+               mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                btn("7"),
+                btn("8"),
+                btn("9"),
+                btn("*"),
+            ],
+            ),
+             Row(
+               mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                btn("0"),
+                btn("/"),
+                ElevatedButton(onPressed: clear, child: Text("clear")),
+                ElevatedButton(onPressed: output, child: Text("=")),
+            ],
+            ),
+        
           ],
         ),
       ),
@@ -61,3 +95,4 @@ class _HomeState extends State<Home> {
     );
   }
 }
+
